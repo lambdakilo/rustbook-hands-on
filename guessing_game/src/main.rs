e62ca9d1c45e561bbf044869fbd2f3f33c97f665
Akira Taguchi <31825085+lambdakilo@users.noreply.github.com>
2026-07-10 13:51:03 +0300

    Finish chapter 2

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,42 @@
+use std::cmp::Ordering;
+use std::io;
+
+use rand::Rng;
+
+fn recurse(n: u64) -> u64 {
+    if n == 0 { 0 } else { recurse(n - 1) + 1 }
+}
+
+fn main() {
+    println!("{}", recurse(10_000));
+
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
